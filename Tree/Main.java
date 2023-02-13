cea711c8c2ffbe27f6eddec063f6d22053f72084
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-02-13 11:12:46 +0530

    tree custom implementation

---
+++ Tree/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    static class Node{
+        int value;
+        Node left,right;
+        Node(int val){
+            this.value = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        addNode(2, root, 'L','L');
+        addNode(3, root, 'R','R');
+        addNode(6, root, 'L','L');
+        addNode(7, root, 'L','R');
+        inOrder(root);
+        // System.out.println();
+        // inOrder(root);
+        // System.out.println();
+        // postOrder(root);
+        // System.out.println();
+
+    }
+
+    public static void preOrder(Node root){
+        if(root !=null){
+            System.out.print(root.value+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+
+        }
+    }
+    public static void postOrder(Node root){
+        if(root !=null){
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.value+" ");
+        }
+    }
+
+    public static void inOrder(Node root){
+        if(root !=null){
+            inOrder(root.left);
+            System.out.print(root.value+" ");
+            inOrder(root.right);
+        }
+    }
+
+    public static void addNode(int value,Node root,char dir1 , char dir2){
+
+        Node temp = root;
+        if(dir1 == 'L'){
+            while(temp.left !=null){
+                temp=temp.left;
+            }
+        }else{
+            while(temp.right != null){
+                temp = temp.right;
+            }
+        }
+        if(dir2=='L'){
+            temp.left = new Node(value);
+        }else{
+            temp.right = new Node(value);
+        }
+    }
+}
+
+
+
+
+
+
+
+
