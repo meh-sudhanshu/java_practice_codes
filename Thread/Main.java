2883415dcce97499d1e80c039b9bc8dc4aa32ff1
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-02-15 10:55:36 +0530

    implementing thread using runnable

--- Thread/Main.java
+++ Thread/Main.java
@@ -1,12 +1,52 @@
-public class Main {
 
-    public static class Test extends Thread{
-        public void run(){
-            System.out.println("my thread is running");
-        }
-    }
-    public static void main(String[] args) {
-        Test t = new Test();
-        t.start();
+// class Main extends Thread{
+//     public void run(){
+//         System.out.println("Thread "+Thread.currentThread().getId()+" is running");
+//     }
+
+
+//     public static void main(String[] args) {
+//         for(int i = 1;i<=5;i++){
+//             Main t = new Main();
+//             t.start();
+//         }
+//         for(int j=0;j<=5;j++){
+//             System.out.println(j+"  ");
+//         }
+//     }
+// }
+
+class ThreadDemo implements Runnable{
+    public void run(){
+        System.out.println("current thread id is "+Thread.currentThread().getId());
     }
 }
+class Main{
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            Thread obj = new Thread(new ThreadDemo());
+            obj.start();
+        }
+
+        for(int j=0;j<5;j++){
+            System.out.println(j);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
