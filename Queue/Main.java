75a846ad0d30059fdde3208a5415c11edc22f782
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-02-16 10:17:20 +0530

    queue implemenation using arraylist

---
+++ Queue/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> queue = new ArrayList<>();
+        push(1,queue);
+        push(2,queue);
+        push(3,queue);
+        printQueue(queue);
+        pop(queue);
+        printQueue(queue);
+    }
+
+    public static void pop(ArrayList<Integer> queue){
+        if(queue.size() == 0){
+            System.out.println("queue is empty");
+        }
+        else
+            System.out.println("removed value is "+queue.remove(0));
+    }
+
+
+    public static int top(ArrayList<Integer> queue){
+        return queue.get(0);
+    }
+    public static void push(int value,ArrayList<Integer> queue){
+        queue.add(value);
+    }
+    public static void printQueue(ArrayList<Integer> queue){
+        for(int e : queue){
+            System.out.print(e+" ");
+        }
+        System.out.println();
+    }
+
+}
