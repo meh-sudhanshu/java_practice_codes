702659dd9df6cd93aee8f433c8050857177c6c51
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-07-15 20:09:15 +0530

    linked list impl

---
+++ Wipro_trainning/15-07-23/LinkedListImpl.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class LinkedListImpl {
+    static class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) {
+    int[] arr = {1,2,4,3,5};
+        Node start = generateList(arr);
+        printList(start);
+    }
+    private static void printList(Node start) {
+        while(start != null){
+            System.out.print(start.value+" ");
+            start = start.next;
+        }
+    }
+    public static Node generateList(int[] arr){
+        Node start = null;
+        Node temp = start;
+
+        for(int e: arr){
+            Node tempNode = new Node(e);
+
+            if(start == null){
+                start = tempNode;
+                temp = start;
+            }else{
+                temp.next = tempNode;
+                temp = temp.next;
+            }
+        }
+
+
+
+        return start;
+    }
+}
