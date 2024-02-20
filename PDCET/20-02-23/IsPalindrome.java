59dc955f925742e312e0a9aed570b5d9cb38a86a
sudhanshu kumar <sudhanshukumar8818@gmail.com>
2024-02-20 11:57:34 +0530

    more codes

---
+++ PDCET/20-02-23/IsPalindrome.java
@@ -0,0 +1,16 @@
+class IsPalindrome{
+    public static void main(String[] args) {
+        String str = "madam";
+        boolean ans = isPalindrome(str);
+        System.out.println(ans);
+    }
+    public static boolean isPalindrome(String str){
+        if (str.length() == 0 || str.length() == 1) {
+            return true;
+        }   
+        if(str.charAt(0) != str.charAt(str.length()-1)){
+            return false;
+        }
+        return isPalindrome(str.substring(1, str.length()-1));
+    }
+}
