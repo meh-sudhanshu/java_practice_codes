a09675cad80082f495caea0736e30fa2fcee8341
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-06-22 12:22:59 +0530

    intersection of two singl linked list impl

--- madurai/S27/22-06-2024/SingLinkedListProblems.java
+++ madurai/S27/22-06-2024/SingLinkedListProblems.java
@@ -33,6 +33,17 @@ class SingleLinkedListProblems{
         }
     }
 
+    public static Node addAtFirst(Node start, char value){
+        Node newNode = new Node(value);
+        if(start == null){
+            start = newNode;
+        }else{
+            newNode.next = start;
+            start = newNode;
+        }
+        return start;
+    }
+
     public static boolean isPalindromic(Node start){
         Stack<Character> st = new Stack<>();
         Node temp = start;
@@ -65,13 +76,46 @@ class SingleLinkedListProblems{
         return start;
     }
 
-    public static void main(String[] args){
-        char[] arr = {'m','a','l','a','y','a','l','a','t','m'};
-        Node start = createList(arr);
-        boolean ans = isPalindromic(start);
-        System.out.println("is list palindromic --> "+ ans);
+    public static char getIntersectionpoint(Node list1, Node list2){
+        Node temp1 = list1;
+        Node temp2 = list2;
+        char ans = '0';
+        while(temp1 != null && temp2 != null){
+            char ch1 = temp1.data;
+            char ch2 = temp2.data;
+            if(ch1 == ch2){
+                if(ans == '0'){
+                    ans = ch1;
+                }
+            }else{
+                ans = '0';
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
 
-        start = reverseList(start);
-        printList(start);
+        return ans;
+    }
+
+    public static void main(String[] args){
+        char[] arr1 = {'m','a','l','a','t','a','l','a','t','m'};
+        char[] arr2 = {'m','p','l','t','y','a','l','a','t','m'};
+        // Node start = createList(arr);
+        // System.out.println("list at the begining");
+        // printList(start);
+        // System.out.println();
+        // start = addAtFirst(start,'L');
+        // System.out.println("list after adding an element");
+        // printList(start);
+        // boolean ans = isPalindromic(start);
+        // System.out.println("is list palindromic --> "+ ans);
+
+        // start = reverseList(start);
+        // printList(start);
+
+        Node list1 = createList(arr1);
+        Node list2 = createList(arr2);
+        char ch = getIntersectionpoint(list1,list2);
+        System.out.println("intersection point is "+ ch);
     }
 }
