c9abdffec1c7676883f89fc5fe3ec752683635d0
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-06-28 09:54:59 +0530

    more codes

---
+++ madurai/S27/28-06-2024/StringProblem.java
@@ -0,0 +1,52 @@
+    
+    
+class StringProblem {
+    public static void main(String[] args) {
+        String str = "madam";
+        printAns(str);
+    }
+
+    private static void printAns(String str) {
+        int[][] arr = new int[str.length()][str.length()];
+        int ri=0,ci=0;
+        int totalCount = 0;
+        int lpssi = 0, lpsei = 0;
+        while(ci < arr.length){
+            int cFlag = ci;
+            while (cFlag < arr.length){
+                if(ri == cFlag){
+                    arr[ri][cFlag] = 1;
+                    totalCount+=1;
+                }else if(Math.abs(ri-cFlag) <=2){
+                    if(str.charAt(ri) == str.charAt(cFlag)){
+                        arr[ri][cFlag] = 1;
+                        totalCount+=1;
+                        if((lpsei- lpssi+1) < Math.abs(cFlag-ri)){
+                            lpssi = ri;
+                            lpsei = cFlag;
+                        }
+                    }
+                }else{
+                    if(str.charAt(ri) == str.charAt(cFlag)){
+                        if( arr[ri+1][cFlag-1] == 1){
+                            arr[ri][cFlag] = 1;
+                            totalCount+=1;
+                            if((lpsei- lpssi+1) < Math.abs(cFlag-ri)){
+                                lpssi = ri;
+                                lpsei = cFlag;
+                            }
+                        }
+                    }
+                }
+                //System.out.print(arr[ri][cFlag]+" ");
+                ri+=1;
+                cFlag+=1;
+            }
+            ri = 0;
+            ci+=1;
+        }
+        System.out.println(totalCount);
+        System.out.println(str.substring(lpssi, lpsei+1));
+
+    }
+}
