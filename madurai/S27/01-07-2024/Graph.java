c2b19f694d829aa880b3fdc43e307a98858bfac3
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-01 11:00:43 +0530

    more codes of graph

--- madurai/S27/01-07-2024/Graph.java
+++ madurai/S27/01-07-2024/Graph.java
@@ -1,15 +1,33 @@
 class Graph{
     public static void main(String[] args) {
-        int[][] edges = {{0,1},{0,2},{1,3},{4,5},{4,7},{5,6},{7,6},{2,3}};
+        int[][] edges = {{0,1},{0,2},{1,3},{3,4},{4,5},{4,7},{5,6},{7,6},{2,3}};
         int n = 8;
         int[][] graph = buildGraph(edges,n);
         //printGraph(graph);
         int[] visited = new int[n];
-        int src = 3;
+        int src = 1;
         int des = 7;
         visited[src] = 1;
-        boolean ans = isPathExist(graph,src,des,visited);
-        System.out.println(ans);
+        //boolean ans = isPathExist(graph,src,des,visited);
+        //System.out.println(ans);
+        printAllPaths(graph,src,des,visited,src+"");
+    }
+
+    private static void printAllPaths(int[][] graph, int src, int des,int[] visited, String psf) {
+        if(src == des){
+            System.out.println(psf);
+            return;
+        }
+        int[] nbrs = graph[src];
+        for(int i=0;i<nbrs.length;i++){
+            if(graph[src][i] == 1){
+                if(visited[i] == 0){
+                    visited[i] = 1;
+                    printAllPaths(graph,i, des, visited, psf+"->"+i);
+                    visited[i] = 0;
+                }
+            }
+        }
     }
 
     private static boolean isPathExist(int[][] graph, int src, int des,int[] visited) {
