872fd8f8f30b1e7cb8bd485a6f051914959855a3
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-01 14:55:01 +0530

    more codes

---
+++ madurai/S22/01-07-2024/Graph.java
@@ -0,0 +1,56 @@
+class Graph{
+    public static void main(String[] args) {
+        int[][] edges = {{0,1},{0,3},{1,2},{2,3},{4,5},{4,7},{5,6},{6,7}};
+        int n = 8;
+        int[][] graph = buildGraph(edges,n);
+        //printGraph(graph);
+        int src = 3;
+        int des = 7;
+        int[] visited = new int[n];
+        boolean ans = hasPath(graph,src,des,visited);
+        System.out.println(ans);
+
+    }
+
+
+
+
+    private static boolean hasPath(int[][] graph, int src, int des,int[] visited) {
+        visited[src] = 1;
+        if(src == des) return true;
+        int[] nbrs = graph[src];
+        for(int i=0;i<nbrs.length;i++){
+            if(graph[src][i] == 1){
+               if(visited[i] == 0){
+                    boolean ans = hasPath(graph, i, des,visited);
+                    if(ans == true) return true;
+               }
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+    private static int[][] buildGraph(int[][] edges,int n) {
+       int[][] graph = new int[n][n];
+       for(int[] edge : edges){
+            int src = edge[0];
+            int des = edge[1];
+            graph[src][des] = 1;
+            graph[des][src] = 1;
+       }
+       return graph;
+    }
+
+    private static void printGraph(int[][] graph) {
+        for(int[] row : graph){
+            for(int e : row){
+                System.out.print(e+" ");
+            }
+            System.out.println();
+        }
+    }
+}
