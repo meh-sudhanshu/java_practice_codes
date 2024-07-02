b8d51278087baa35ab0d269964b6bdaf35f00575
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-02 10:39:31 +0530

    more codes

--- madurai/S27/02-07-2024/Graph.java
+++ madurai/S27/02-07-2024/Graph.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Graph{
     public static void main(String[] args) {
         int[][] edges = {{0,1,5},{0,6,2},{0,2,3},
@@ -8,12 +11,38 @@ class Graph{
         int[][] graph = buildGraph(edges,n);
         int src = 2;
         int des = 9;
-        int minCost = getMinimumCost(graph,src,des);
-        System.out.println(minCost);
+        String pathSorFar = src+"";
+        ArrayList<String> allPaths = new ArrayList<>();
+        int[] visited = new int[n];
+        visited[src] = 1;
+        allPaths = getAllPaths(graph,src,des,pathSorFar,allPaths,visited);
+        System.out.println(allPaths);
+        // int minCost = getMinimumCost(graph,src,des);
+        // System.out.println(minCost);
     }
 
-    private static int getMinimumCost(int[][] graph, int src, int des) {
+    // private static int getMinimumCost(int[][] graph, int src, int des) {
         
+    // }
+
+    private static ArrayList<String> getAllPaths(int[][] graph, int src, 
+                    int des,String psf,ArrayList<String> allPaths,int[] visited) {
+        if(des == src){
+            allPaths.add(psf);
+            return null;
+        }
+        int[] nbrs = graph[src];
+        for(int i=0;i<nbrs.length;i++){
+            if(graph[src][i] != 0){
+                if(visited[i] == 0){
+                    visited[i] = 1;
+                    getAllPaths(graph, i, des, psf+"->"+i, allPaths, visited);
+                    visited[i] = 0;
+                }
+            }
+        }
+
+        return allPaths;
     }
 
     private static int[][] buildGraph(int[][] edges, int n) {
