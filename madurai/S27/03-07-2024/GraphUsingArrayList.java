28c488f74c12e2a51b7440983a7e3762a2b1da45
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-03 12:34:36 +0530

    more codes

--- madurai/S27/03-07-2024/GraphUsingArrayList.java
+++ madurai/S27/03-07-2024/GraphUsingArrayList.java
@@ -14,19 +14,46 @@ class GraphUsingArrayList{
         }
     }
     public static void main(String[] args) {
-        int[][] edges = {{0,1,2},{0,2,4},{1,3,-1},{2,3,3},{3,4,5},{4,5,5},
+        int[][] edges = {{0,1,2},{0,2,4},{1,3,-1},{2,3,3},{4,5,5},
                             {4,6,-1},{6,5,-3}};
         int n = 7;
-        
         ArrayList<Edge>[] graph = buildGraph(edges,n);
+        int src = 0;
+        int des = 6;
+        int[] visited = new int[n];
+        visited[src] = 1;
+        boolean ans = hasPath(graph,src,des,visited);
+        System.out.println(ans);    
+    }
+    private static boolean hasPath(ArrayList<GraphUsingArrayList.Edge>[] graph, int src, int des, int[] visited) {
+       if(src == des){
+            return true;
+       }
+       ArrayList<Edge> nbrs = graph[src];
+       for(Edge edge : nbrs){
+            int nbr = edge.des;
+            if(visited[nbr] == 0){
+                visited[nbr] = 1;
+                boolean ans = hasPath(graph, nbr, des, visited);
+                // if(ans == true) return true;
+                visited[nbr] = 0;
+            }
+       }
+
+       return false;
+    }
+
+
+    private static void printGraph(ArrayList<Edge>[] graph){
         for(int i=0;i<graph.length;i++){
             for(int j=0;j<graph[i].size();j++){
                 System.out.print(graph[i].get(j).src+"->"+graph[i].get(j).des+"->"+graph[i].get(j).wt+" , ");
             }
             System.out.println();
         }
-                
     }
+
+
     private static ArrayList<Edge>[] buildGraph(int[][] edges, int n){
         ArrayList<Edge>[] graph = new ArrayList[n];
         for(int i=0;i<graph.length;i++){
