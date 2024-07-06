05041e0f46b3c056aaa6aa04ccbec2bbad89920a
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-06 16:18:07 +0530

    More codes

---
+++ madurai/S22/06-07-2024/NumberOfIsland.java
@@ -0,0 +1,28 @@
+public class NumberOfIsland {
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] matrix  = {{1,0,0,0,1},{1,1,1,1,1},{1,0,0,1,0},{1,1,1,1,0},{1,1,0,0,0}};
+        int[][] visited = new int[n][n];
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 0 && visited[i][j] == 0){
+                    expandNode(matrix,i,j,visited,n);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void expandNode(int[][] matrix, int i, int j, int[][] visited,int n) {
+        if(i<0 || j< 0 || i>= n || j>=n  || matrix[i][j] == 1 || visited[i][j] == 1){
+            return;
+        }
+        visited[i][j] = 1;
+        expandNode(matrix, i-1, j, visited,n);
+        expandNode(matrix, i+1, j, visited,n);
+        expandNode(matrix, i, j-1, visited,n);
+        expandNode(matrix, i, j+1, visited,n);
+    }    
+}
