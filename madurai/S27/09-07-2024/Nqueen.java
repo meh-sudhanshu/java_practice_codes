eeed179d83cbbb178be478b3266fbd24ff351d3b
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-09 11:57:38 +0530

    more codes

--- madurai/S27/09-07-2024/Nqueen.java
+++ madurai/S27/09-07-2024/Nqueen.java
@@ -9,20 +9,22 @@ public class Nqueen {
     }
 
     private static void printAllConfigs(int[][] board, int n, int[] col, int[] diagonal, int[] revDaigonal, String csf, int row) {
-        if(row == n-1){
+        if(row == n){
             System.out.println(csf);
             return;
         }
-        for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
             // i = row
-            if(col[j] == 0 && diagonal[(row-j)+n] == 0 && revDaigonal[row+j] == 0){
-                col[j] = 1;
-                diagonal[(j-row)+n] = 1;
-                revDaigonal[row+j] = 1;
-                printAllConfigs(board, n, col, diagonal, revDaigonal, csf+","+row+"->"+j , row+1);
-                col[j] = 0;
-                diagonal[(j-row)+n] = 0;
-                revDaigonal[row+j] = 0;
+            if(col[i] == 0 && diagonal[row-i+n] == 0 && revDaigonal[row+i] == 0){
+                col[i] = 1;
+                diagonal[row-i+n] = 1;
+                revDaigonal[row+i] = 1;
+                printAllConfigs(board, n, col, diagonal, revDaigonal,
+                    csf == "" ? row+"->"+i : csf+","+row+"->"+i 
+                , row+1);
+                col[i] = 0;
+                diagonal[row-i+n] = 0;
+                revDaigonal[row+i] = 0;
             }
         }
     }    
