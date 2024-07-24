84c7e73568312aa339c7ca3fc4c772afba2f150d
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-24 20:06:45 +0530

    more codes

---
+++ Chehnitha/22-07-2024/MyFirstJavaCode.java
@@ -0,0 +1,13 @@
+class MyFirstJavaCode{
+    public static void main(String sudhanshu[]){
+        System.out.println("hello World");
+    }
+}
+
+
+// data types ---> typecasting , identifiers , reserve keywords
+// number syatem , conversion
+
+
+// byte , short, int , long
+
---
+++ Chehnitha/23-07-2024/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        // int[]  arrayWithHardcodedValues = {1,2,3,5,6};
+        // System.out.println(arrayWithHardcodedValues);
+
+        // int[] arrayWithDynamicValues = new int[3];
+        // System.out.println(arrayWithDynamicValues[2]);
+
+
+        // float[] arrayWithFloatingPointValues = new float[10];
+
+        // arrayWithFloatingPointValues[3] = (float)1.2;
+        // System.out.println(arrayWithFloatingPointValues[1]);
+        // System.out.println(arrayWithFloatingPointValues[3]);
+
+        // double d = 11.2765;
+        // int a = (int)d;
+        // System.out.println(a);
+
+        // char ch = 'a';
+        // int asciiValue = (int)ch;
+        // System.out.println(asciiValue);
+
+        // int a1 = 970000;
+        // char ch1 = (char)a1;
+        // System.out.println(ch1);
+
+
+        // int[] arr = {1,2,9,45,34,65,37,90};
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[4]);
+        int i=0;
+        int n = 10;
+        for(;i<=n;){
+            System.out.println(i);
+            i+=1;
+        }
+
+        // methods on arrays in java
+
+    }
+}
