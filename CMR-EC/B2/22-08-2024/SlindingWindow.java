efbbc681dcf3e146bc5c23ff90937c35b2c38f05
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-08-22 11:52:56 +0530

    more codes

---
+++ CMR-EC/B2/22-08-2024/SlindingWindow.java
@@ -0,0 +1,32 @@
+public class SlindingWindow {
+    public static void main(String[] args) {
+        int[] arr = {-3,4,8,5,-6,-3,4,9,-30};
+        int ws = 3;
+
+        int ans = getMaximumSubArraySumOfSizeK(arr,ws);
+        System.out.println(ans);
+    }
+    private static int getMaximumSubArraySumOfSizeK(int[] arr,int ws){
+        int ans = Integer.MIN_VALUE;
+        int previousSum = 0;
+        int i = 0, j = ws-1;
+
+        while (j < arr.length) {
+
+            int currentSum = 0;
+            if (i == 0) {
+                for(int l = i;l<=j;l++) currentSum+=arr[l];
+                previousSum = currentSum;
+            }else{
+                currentSum = previousSum - arr[i-1] + arr[j];
+                previousSum = currentSum;
+            }
+            if (currentSum > ans) {
+                ans = currentSum;
+            }
+            i+=1;
+            j+=1;
+        }
+        return ans;
+    }
+}
--- CMR-EC/B2/22-08-2024/TargetSum.java
+++ CMR-EC/B2/22-08-2024/TargetSum.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 
 class TargetSum{
     public static void main(String[] args) {
-        int[] arr = {2,3,-2,4,6,7,-9,10,6,2};
-        int target = 700;
+        int[] arr = {2,3,-2,4,6,7,-9,10,6,2}; 
+        int target = 700
+        ;
 
         boolean ans = isPairExist(arr,target);
         System.out.println(ans);
