537c9f3be2a4068a5626cac8ebf998bb4b056470
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-09-18 10:54:08 +0530

    more codes

---
+++ CMR-EC/B5/18-09-2023/Main.java
@@ -0,0 +1,19 @@
+class Main{
+    public static void main(String[] args){
+        int a = 200;
+        switch (a) {
+           case 10:
+                System.out.println("case 10");
+                break;
+           case 20:
+                System.out.println("case 20");
+                break;
+           case 30:
+            System.out.println("case 30");
+            break;
+           default:
+            System.out.println("This is a defaut case");
+
+        }
+    }
+}
