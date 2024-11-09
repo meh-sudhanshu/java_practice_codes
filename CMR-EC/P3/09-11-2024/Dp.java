8cf962e7c31a94d8731384ee7683b282b2d3ea2d
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-11-09 11:39:44 +0530

    more codes

---
+++ CMR-EC/P3/09-11-2024/Dp.java
@@ -0,0 +1,27 @@
+class Dp{
+    public static void main(String[] args) {
+        int n = 80;
+        int[] dp = new int[n+1];
+        int ans = fun(n,dp);
+        System.out.println(ans);
+    }
+    private static int fun(int n,int[] dp){
+        if (n <= 4) {
+            if(n >=0){
+                dp[n] = n;
+            }
+            return n;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        System.out.println("function is invoked with "+ n);
+        int ans1 = fun(n-2, dp);
+        dp[n-2] = ans1;
+        int ans2 = fun(n-4, dp);
+        dp[n-4] = ans2;
+        return ans1+ans2;
+    }
+}
