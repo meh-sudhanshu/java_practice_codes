b4021734ccdd141a8078763b286f99f11b3b8898
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-11-14 12:23:41 +0530

    more code

---
+++ CMR-EC/c1/SubStrLength.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class SubStrLength {
+    public static void main(String[] args) {
+        String str = "malayalam";
+        int ans = findLengthOfSubstringWithAllDistinctElement(str);
+        System.out.println(ans);
+    }
+    private static int findLengthOfSubstringWithAllDistinctElement(String str){
+        HashMap<Character,Integer> myMap = new HashMap<>();
+        int ans = Integer.MIN_VALUE;
+        int i =0, j = 0;
+        while (j < str.length()) {
+            char ch = str.charAt(j);
+            if (!myMap.containsKey(ch)) {
+                myMap.put(ch,j);
+            }else{
+                int currIndex = myMap.get(ch);
+                if (currIndex < i) {
+                    myMap.put(ch,j);
+                }else{
+                    int currLength = (j-i);
+                    if (currLength > ans) {
+                        ans = currLength;
+                    }
+                    myMap.put(ch,currIndex+1);
+                    i = currIndex+1;
+                }
+               
+            }
+            j+=1;
+        }
+        return Math.max(j-i,ans);
+    }
+}
