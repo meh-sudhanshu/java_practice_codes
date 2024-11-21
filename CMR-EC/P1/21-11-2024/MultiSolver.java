4f56315d9eedcea4fbcde5281983402610f67553
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-11-21 14:35:43 +0530

    more codes

--- CMR-EC/P1/21-11-2024/MultiSolver.java
+++ CMR-EC/P1/21-11-2024/MultiSolver.java
@@ -1,13 +1,30 @@
-public class MultiSolver {
+class MultiSolver {
     public static void main(String[] args) {
-        int[][] arr = {{3,-1,2,9},{0,8,7,6},{5,-2,3,-2},{8,-1,9,6}};
-        int n = 4;
+        String str = "axaxyyxaz";
+        int n = str.length();
+        int[][] dp = new int[n][n];
         int j = 0;
+        int count = 0;
         while (j < n) {
             int i = 0;
             int jFlag = j;
             while (jFlag < n) {
-                System.out.print(arr[i][jFlag]+" ");
+                if (i == jFlag) {
+                    dp[i][jFlag]  = 1;
+                    count+=1;
+                }else if ( Math.abs(i - jFlag) <= 2){
+                    if(str.charAt(i) == str.charAt(jFlag)){
+                        dp[i][jFlag] = 1;
+                        count+=1;
+                    }
+                }else{
+                    if(str.charAt(i) == str.charAt(jFlag)){
+                        if(dp[i+1][jFlag-1] == 1){
+                            dp[i][jFlag] = 1;
+                            count+=1;
+                        }
+                    }
+                }
                 i+=1;
                 jFlag+=1;
             }
@@ -15,5 +32,6 @@ public class MultiSolver {
 
             j+=1;
         }
+        System.out.println(count);
     }
 }
