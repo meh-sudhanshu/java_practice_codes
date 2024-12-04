b094ebb667f51467851bdf10cef7a5db321310fe
mohini12345 <mohinibansal1997@gmail.com>
2024-12-04 12:21:47 +0530

    more codes

---
+++ stanley/ece-eee/04-12-2024/Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Reverse{
+    public static void main(String[] sudhanshu) {
+        int[] arr = {2,7,3,6};
+        int i = 0, j = arr.length-1;
+        while (i<j) {
+            int temp = arr[j];
+            arr[j]= arr[i];
+            arr[i] = temp;
+            i+=1;
+            j-=1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
