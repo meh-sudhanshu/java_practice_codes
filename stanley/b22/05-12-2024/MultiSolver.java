fe623bc47b19cc8b03b97fb4e8d5708c4a5da37b
mohini12345 <mohinibansal1997@gmail.com>
2024-12-05 12:56:54 +0530

    More codes

--- stanley/b22/05-12-2024/MultiSolver.java
+++ stanley/b22/05-12-2024/MultiSolver.java
@@ -5,6 +5,8 @@ class MultiSolver{
         int[][] dp = new int[n][n];
         int j = 0;
         int totalNumberOfPalindromicSubstring = 0;
+        String largest = "";
+        String smallest = "";
         while (j < n) {
             int i = 0;
             int jf = j;
@@ -12,16 +14,25 @@ class MultiSolver{
                 if (i == jf) {
                     dp[i][jf] = 1;
                     totalNumberOfPalindromicSubstring+=1;
+                    largest = str.substring(i, jf+1);
                 }else if(Math.abs(i-jf) <=2){
                     if(str.charAt(i) == str.charAt(jf)){
                         dp[i][jf] = 1;
                         totalNumberOfPalindromicSubstring+=1;
+                        largest = str.substring(i, jf+1);
+                        if(smallest.equals("")){
+                            smallest = str.substring(i, jf+1);
+                        }
                     }
                 }else{
                     if(str.charAt(i) == str.charAt(jf)){
                         if(dp[i+1][jf-1] == 1){
                             dp[i][jf] = 1;
                             totalNumberOfPalindromicSubstring+=1;
+                            largest = str.substring(i, jf+1);
+                            if(smallest.equals("")){
+                                smallest = str.substring(i, jf+1);
+                            }
                         }
                     }
                 }
@@ -31,5 +42,7 @@ class MultiSolver{
             j+=1;
         }
         System.out.println(totalNumberOfPalindromicSubstring);
+        System.out.println(smallest);
+        System.out.println(largest);
     }
 }
