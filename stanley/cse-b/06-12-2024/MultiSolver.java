aee0017803fd2e5ad79e2e7e265913edd8db30ee
mohini12345 <mohinibansal1997@gmail.com>
2024-12-06 12:27:43 +0530

    more codes

--- stanley/cse-b/06-12-2024/MultiSolver.java
+++ stanley/cse-b/06-12-2024/MultiSolver.java
@@ -2,22 +2,34 @@ import java.util.Arrays;
 
 class MultiSolver{
     public static void main(String[] args) {
-        int[][] arr = {
-            {1,8,2,6,7},
-            {3,7,9,22,55},
-            {7,33,78,65,3},
-            {3,5,2,6,7},
-            {98,5,6,2,3}
-        };
-        int n = arr.length;
+        String str = "malayalam";
+        int n = str.length();
+        int countOfPalindromicSubstring = 0;
+        int[][] dp = new int[n][n];
         int j = 0;
         while (j < n) {
             int row = 0, col = j;
             while (col < n) {
-                System.out.print(arr[row][col]+" ");
+                if(row == col){
+                    dp[row][col] = 1;
+                    countOfPalindromicSubstring+=1;
+                }else if(col-row <=2 ){
+                    if(str.charAt(row) == str.charAt(col)){
+                        countOfPalindromicSubstring+=1;
+                        dp[row][col] = 1;
+                    }
+                }else{
+                    if(str.charAt(row) == str.charAt(col)){
+                        if(dp[row+1][col-1] == 1){
+                            dp[row][col] = 1;
+                            countOfPalindromicSubstring+=1;
+                        }
+                    }
+                }
                 row+=1; col+=1;
             }
             j+=1;
         }
+        System.out.println(countOfPalindromicSubstring);
     }
 }
